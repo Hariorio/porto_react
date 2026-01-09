43ac2989471a3a5b8552be0c3ab7e94bf0f5d674
Hariorio <hariotrihadiprastya@gmail.com>
2026-01-09 16:44:52 +0700

    coba2 variasi data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,50 @@
+import { useState } from 'react';
+
+
+
 const HomePage = () => {
-  return (
-    <div>Home Page</div>
+ const [users, setUsers] = useState([
+    { id: 1, name: "Ade" },
+    { id: 2, name: "Budi" },
+  ]);
+  
+  function handleEdit(user) {
+    console.log('Edit user:', user);
+  }
+
+return (
+    <div>
+      <div>Home Page</div>
+       <h4>Data User</h4>
+      <table border="1" cellPadding="8">
+        <thead>
+          <tr>
+            <th>ID</th> 
+            <th>Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>
+                <button onClick={() => handleEdit(user)}>Edit</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+
+
+       {/* {users.map((user) => (
+        <span key={user.id} style={{ marginRight: 12 }}>
+          Nama: {user.name}
+        </span>
+      ))} */}
+    </div>  
   )
 }
 
