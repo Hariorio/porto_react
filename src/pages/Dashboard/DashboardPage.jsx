edb17d99b1c3dec9e105f63ab770b59a5a15f004
Hariorio <hariotrihadiprastya@gmail.com>
2026-01-26 16:33:59 +0700

    ngumpulin niat tp masih g niat

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -7,6 +7,10 @@ const DashboardPage = () => {
         <h2 className="card-title">Login</h2>
         <p className="card-muted">Ini card versi @apply global css.</p>
        <div className="avatar-circle">AA</div>
+        <div className="avatar-circle">AA</div>
+         <div className="avatar-circle">AA</div>
+          <div className="avatar-circle">AA</div>
+           <div className="avatar-circle">AA</div>
       </div>
       
    
